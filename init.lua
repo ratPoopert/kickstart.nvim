f0ad3cd51bb436c3a22956e8c8fabe414b8a6285
Patrick Ruppert <patrick.j.ruppert@gmail.com>
2025-05-21 20:54:00 -0400

    Import custom config and keymaps

--- init.lua
+++ init.lua
@@ -1016,5 +1016,8 @@ require('lazy').setup({
   },
 })
 
+require 'custom/keymaps'
+require 'custom/config'
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
