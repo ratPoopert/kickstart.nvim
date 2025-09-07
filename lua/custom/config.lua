7ea5500083549189279b7910b797af09b05a0d3d
Patrick Ruppert <patrick.j.ruppert@gmail.com>
2025-09-06 22:18:53 -0400

    Open help files in vertical split

--- lua/custom/config.lua
+++ lua/custom/config.lua
@@ -1,2 +1,9 @@
 vim.wo.wrap = true
 vim.wo.linebreak = true
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'help',
+  callback = function()
+    vim.cmd 'wincmd L' -- Move help window to the right (vertical split)
+  end,
+})
